48f70ad771b94c74f5a4de420999b02ffe47e07a
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-04-02 22:41:38 +0300

    new views created & some views removed

---
+++ src/views/Blog.vue
@@ -0,0 +1,23 @@
+<script>
+import BaseLayout from '@/components/Base/BaseLayout.vue';
+import BlogList from '../components/Blog/BlogList.vue';
+
+export default {
+    name: 'Blog',
+    components: { BaseLayout, BlogList },
+}
+</script>
+
+<template>
+    <base-layout>
+        <section class="section py-100 mt-100 mt-mobile-50">
+            <div class="container">
+                <div class="text-center">
+                    <h2>blog</h2>
+                    <p class="color-grey">son güncelleme: 02.04.2021</p>
+                </div>
+                <blog-list />
+            </div>
+        </section>
+    </base-layout>
+</template>
---
+++ src/views/OpenSource.vue
@@ -0,0 +1,30 @@
+<script>
+import BaseLayout from '@/components/Base/BaseLayout.vue'
+import OpenSourceList from '@/components/OpenSource/OpenSourceList.vue'
+
+export default {
+  components: { BaseLayout, OpenSourceList },
+    //
+}
+</script>
+
+<template>
+    <base-layout>
+        <section class="section py-100 mt-100 mt-mobile-50">
+            <div class="container">
+                <div class="text-center">
+                    <h2>
+                        open source
+                        <i class="fas fa-heart fa-xs color-dark"></i>
+                    </h2>
+                    <p class="color-grey">Bu sayfada listelenenlerin tümüne <a href="#">github</a>
+                        hesabımdan ulaşabilir,<br>
+                        kullanabilir
+                        ve geliştirmemde
+                        yardımcı olabilirsiniz.</p>
+                </div>
+                <open-source-list />
+            </div>
+        </section>
+    </base-layout>
+</template>
