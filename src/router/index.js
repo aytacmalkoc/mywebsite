8e0c14063540fbeceec2f478e33ab171ea081aff
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-04-02 22:41:52 +0300

    new routes added

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Blog from "../views/Blog.vue";
+import BlogDetail from "../views/BlogDetail.vue";
+import OpenSource from "../views/OpenSource.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/blog",
+    name: "Blog",
+    component: Blog,
+  },
+  {
+    path: "/blog/:slug",
+    name: "BlogDetail",
+    component: BlogDetail,
+  },
+  {
+    path: "/open-source",
+    name: "OpenSource",
+    component: OpenSource,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
