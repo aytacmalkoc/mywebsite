316110d5ce6b4950df706234eef953eca5f32029
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-04-02 22:31:49 +0300

    added base layout for views

---
+++ src/components/Base/BaseLayout.vue
@@ -0,0 +1,25 @@
+<script>
+import Header from '@/components/Header/Header.vue';
+import Footer from '../Footer/Footer.vue';
+
+export default {
+    name: 'BaseLayout',
+    components: {
+        Header,
+        Footer
+    }
+}
+</script>
+
+<template>
+    <div id="wrapper" class="clearfix">
+        <Header />
+
+        <div id="main-content">
+            
+            <slot />
+
+            <Footer />
+        </div>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import BaseLayout from "./components/Base/BaseLayout.vue";
+
+const app = createApp(App)
+  .use(store)
+  .use(router)
+  .mount("#app");
+
+app.component("base-layout", BaseLayout);
