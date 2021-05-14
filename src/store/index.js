4cfbe651b56671eac28f97295626d1c61f31c5e4
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-05-14 08:18:47 +0300

    added fetch repositories action

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,18 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
+  state: {},
+  getters: {},
+  mutations: {},
   actions: {
+    fetchRepositories: async ({ commit }) => {
+      return await axios
+        .get("https://api.github.com/users/aytacmalkoc/repos?sort=created")
+        .then((res) => {
+          return res.data;
+        })
+        .catch((err) => console.log(err));
+    },
   },
-  modules: {
-  }
-})
+});
