0640caeaee9118679a8c635e34788caab0631edf
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-05-14 08:22:06 +0300

    🌈

--- src/components/Blog/BlogList.vue
+++ src/components/Blog/BlogList.vue
@@ -1,12 +1,12 @@
 <script>
-import BlogItem from './BlogItem.vue'
+import BlogItem from "./BlogItem.vue";
 export default {
   components: { BlogItem },
-}
+};
 </script>
 
 <template>
-    <div class="row mt-50">
-        <blog-item />
-    </div>
+  <div class="row mt-50">
+    <blog-item />
+  </div>
 </template>
