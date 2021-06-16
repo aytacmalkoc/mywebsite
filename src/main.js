b0b9cf02ac7d00000bdc61adabe7c34fc0584993
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-06-16 15:57:12 +0300

    added load-script plugin

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import mixin from "./mixin";
-
+import LoadScript from "vue-plugin-load-script";
 import BaseLayout from "./components/Base/BaseLayout.vue";
 
 const app = createApp(App)
   .use(store)
   .use(router)
+  .use(LoadScript)
+  .component("base-layout", BaseLayout)
   .mixin(mixin)
   .mount("#app");
-
-app.component("base-layout", BaseLayout);
